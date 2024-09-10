0d60e6e8ce4ff80ba4cb3f0bba6e915cbede3cc6
AbdelrhmanSerag <141841923+AbdelrhmanSerag@users.noreply.github.com>
2024-09-10 23:21:52 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,3 @@
 <template>
-  <login/>
+  <router-view/>
 </template>
-
-<script setup>
-  import helloVue from './components/helloPage.vue';
-  import login from './components/login.vue';
-  import signUp from './components/sign-up.vue';
-  import product from './components/product.vue';
-  import userPage from './components/userPage.vue';
-  import AdminPage from './components/adminPage.vue';
-  import cartVue from './components/cart.vue';
-  
-  
-  
-
-  
-</script>
-
-<style scoped>
-
-</style>
